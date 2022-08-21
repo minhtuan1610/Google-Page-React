0c25a58f99bd3865ea0b93263e8f15cc691fadc9
minhtuan <tuannm1610@gmail.com>
2022-08-21 18:19:15 +0700

    tao folder img

---
+++ src/components/Main/img/Img.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Img.css';
+import logo from '../../../assets/img/logo.png';
+
+const Img = () => {
+    return (
+        <img alt="" src={logo}/>
+    );
+}
+export default Img;
