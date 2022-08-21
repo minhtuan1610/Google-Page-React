4d263a0ecec9f6772df5b2d10874872e2d8a8589
minhtuan <tuannm1610@gmail.com>
2022-08-21 18:27:25 +0700

    creat the folder structure in Main.js

---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Main.css';
+import Img from 'img/Img';
+import Form from 'form/Form';
+import I18N from 'i18n/I18N';
+
+function Main() {
+    return (
+        <div>
+            <Img/>
+            <Form/>
+            <I18N/>
+        </div>
+    );
+}
+
+export default Main;
