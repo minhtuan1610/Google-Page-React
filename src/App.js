ed97eaf46396ec0e49e97e833b0b2671c5c9fee9
minhtuan <tuannm1610@gmail.com>
2022-08-21 18:15:33 +0700

    Re-create folder structure in App.js,and add file Header.js va .css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './App.css';
-import Navbar from './components/Header/navbar/Navbar';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
-            <Navbar/>
+            <Header/>
+            <Main/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css';
+import Navbar from './navbar/Navbar';
+
+function Header() {
+    return (
+        <Navbar/>
+    );
+}
+
+export default Header;
