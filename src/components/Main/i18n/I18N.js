601f1ab8f5b2b6d4b1fe0647aeab5cde340f6c1a
minhtuan <tuannm1610@gmail.com>
2022-08-21 19:36:08 +0700

    tao va chinh sua chi tiet file I18N .js .css

--- src/components/Main/i18n/I18N.js
+++ src/components/Main/i18n/I18N.js
@@ -2,6 +2,12 @@ import React from "react";
 import './I18N.css';
 
 const I18N = () => {
-
+    return (
+        <div className="lang">
+            Google offered in:
+            <a href="#">Tiếng Việt</a>
+            <a href="#">Fran&ccedil;ais</a>
+        </div>
+    );
 }
 export default I18N;
