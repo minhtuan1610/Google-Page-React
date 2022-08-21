f6b706a1f8f568c723a3f7b07158cd366dd92b76
minhtuan <tuannm1610@gmail.com>
2022-08-21 19:38:43 +0700

    create the folder structure and complete it

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './Main.css';
-import Img from 'img/Img';
-import Form from 'form/Form';
-import I18N from 'i18n/I18N';
+import Img from './img/Img';
+import Form from './form/Form';
+import I18N from './i18n/I18N';
 
 function Main() {
     return (
-        <div>
+        <div className="main">
             <Img/>
             <Form/>
             <I18N/>
