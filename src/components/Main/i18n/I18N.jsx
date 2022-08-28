a071891eeec7f143d865d99cb26c4c87c8700aed
minhtuan <tuannm1610@gmail.com>
2022-08-29 01:54:45 +0700

    edit file I18N.jsx

--- src/components/Main/i18n/I18N.jsx
+++ src/components/Main/i18n/I18N.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './I18N.css';
+import A from "../../UI/a/A";
 
 const I18N = () => {
     return (
         <div className="lang">
             Google offered in:
-            <a href="#">Tiếng Việt</a>
-            <a href="#">Fran&ccedil;ais</a>
+            <A label={"Tiếng Việt"}/>
+            <A label={'Fran{&ccedil;}ais'}/>
         </div>
     );
 }
