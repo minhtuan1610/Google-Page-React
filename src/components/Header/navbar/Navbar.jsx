88e5ffe7bee69cfe89f66e6b32fa1b95a563cd07
minhtuan <tuannm1610@gmail.com>
2022-08-29 01:55:15 +0700

    edit file Navbar.jsx & .css

--- src/components/Header/navbar/Navbar.jsx
+++ src/components/Header/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import './Navbar.css';
-import gMenu from '../../UI/img/g-menu.PNG';
+import gMenu from '../../../assets/img/g-menu.PNG';
+import Button from "../../UI/button/Button";
+import A from "../../UI/a/A";
+import Img from "../../UI/img/Img";
 
 const Navbar = (props) => {
     return (
         <div className="n-wrapper">
-            <a href="#">Gmail</a>
-            <a href="#">Images</a>
-            <img alt="" src={gMenu}/>
-            <button onClick={props.transitPage}>Sign in</button>
+            <A label={"Gmail"}/>
+            <A label={"Images"}/>
+            <Img src={gMenu}/>
+            <Button label={"Sign in"} handleClick={props.transitPage}/>
         </div>
     );
 }
