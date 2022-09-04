87bf5d61718f1fb169e8cf64e2d851d5f72a0a8d
minhtuan <tuannm1610@gmail.com>
2022-09-05 00:08:13 +0700

    add an onChange property to Input component

--- src/components/UI/input/Input.jsx
+++ src/components/UI/input/Input.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './Input.css';
 
-function Input({className, type, value, placeholder}) {
+function Input({className, type, value, placeholder, onChange}) {
     return (
-        <input className={className} type={type} value={value} placeholder={placeholder}/>
+        <input onChange={onChange} className={className} type={type} value={value} placeholder={placeholder}/>
     );
 
 }
