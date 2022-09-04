ed66bbc9a8858fcc546436a706358d21728078a5
minhtuan <tuannm1610@gmail.com>
2022-09-04 15:46:21 +0700

    edit the algorithm of app at Navbar component

--- src/components/Header/navbar/Navbar.jsx
+++ src/components/Header/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import gMenu from '../../../assets/img/g-menu.PNG';
 import Button from "../../UI/button/Button";
 import A from "../../UI/a/A";
 import Img from "../../UI/img/Img";
+// import avatar from '../../../assets/img/avatar.jpg';
 
 const Navbar = (props) => {
     return (
@@ -11,7 +12,10 @@ const Navbar = (props) => {
             <A label={"Gmail"}/>
             <A label={"Images"}/>
             <Img src={gMenu}/>
-            <Button label={"Sign in"} handleClick={props.transitPage}/>
+            {
+                (props.isLoggedIn === true) ? <Button children={"Logged in"} handleClick={props.isLoggedOut}/> :
+                    <Button children={"Sign in"} handleClick={props.goLoginPage}/>
+            }
         </div>
     );
 }
