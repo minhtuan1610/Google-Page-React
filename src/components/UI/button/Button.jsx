d70d4a402dad32012b434df8cc7fbf1767a86c81
minhtuan <tuannm1610@gmail.com>
2022-09-04 15:43:45 +0700

    edit label property to children property at Button component

--- src/components/UI/button/Button.jsx
+++ src/components/UI/button/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './Button.css';
 
-const Button = ({label, handleClick, className}) => {
+const Button = ({children, handleClick, className}) => {
     return (
-        <button onClick={handleClick} className={className}>{label}</button>
+        <button onClick={handleClick} className={className}>{children}</button>
     );
 }
 export default Button;
